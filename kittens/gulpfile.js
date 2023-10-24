6a90f63ea9e1619086327579fc82dfcbf8a4ab63
moha-02 <119495982+moha-02@users.noreply.github.com>
2023-10-24 19:42:09 +0200

    final

--- kittens/gulpfile.js
+++ kittens/gulpfile.js
@@ -1,53 +1,47 @@
 const { series, src, dest, parallel,watch } = require('gulp');
 const scss = require('gulp-sass')(require('sass'));
+const limpiarCSS = require('gulp-clean-css');
+const limpiarJS = require('gulp-uglify');
+const concat = require('gulp-concat');
+
 
 
 function compilaSCSS(){
     return src('./sass/application.scss')
     .pipe(scss())
-    .pipe(dest('./css'))
+    .pipe(dest('./css'));
 }
-
-exports.compilaSCSS = compilaSCSS;
-
-/*
-const limpiarCSS = require('gulp-clean-css');
-const limpiarJS = require('gulp-uglify');
-*/
-/*
-// The `clean` function is not exported so it can be considered a private task.
-// It can still be used within the `series()` composition.
-function clean(cb) {
-  // body omitted
-  cb(); 
-}
-
-// The `build` function is exported so it is public and can be run with the `gulp` command.
-// It can also be used within the `series()` composition.
-function build(cb) {
-  // body omitted
-  cb();
+function watcher(){
+    watch('./sass/application.scss',compilaSCSS);
 }
 
 function minimizaCSS() {
-  return src('src/css/*.css').pipe(limpiarCSS()).pipe(dest('dist/'));
+    return src('./css/*.css').pipe(limpiarCSS()).pipe(dest('dist/css'));
 }
 
 function minimizaJS(){
-  return src('src/js/*.js').pipe(limpiarJS()).pipe(dest('dist/'))
+    return src('./js/*.js').pipe(limpiarJS()).pipe(dest('dist/js'));
 }
 
-function watcher(){
-  watch('src/scss/*scss',compilaSCSS);
+function concatCss(){
+    return src('./dist/css/*.css').pipe(concat('all.css')).pipe(dest('./dist/css/'));
 }
 
+function concatJs(){
+    return src('./dist/js/*.js').pipe(concat('all.js')).pipe(dest('./dist/js/'));
+}
 
+/*
+exports.compilaSCSS = compilaSCSS;
+exports.watcher = watcher;
+exports.minifyCSS = minimizaCSS;
+exports.minifyJS = minimizaJS;
+exports.Concatcss = concatCss;
+exports.Concatjs = concatJs;*/
+
+exports.kittens = parallel(series(compilaSCSS,minimizaCSS,concatCss),series(minimizaJS,concatJs));
+exports.watcher = watcher;
+/*
 exports.build = build;
 exports.default = series(clean, build);
-
-
-
-exports.watcher = watcher;
-//exports.minifyCSS = minimizaCSS;
-//exports.minifyJS = minimizaJS;
 exports.minifyAll = parallel(minimizaCSS,minimizaJS);*/
